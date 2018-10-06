f382329125dccf5ad1a68b0574c0d65346f148c8
rohankharwadkar@ufl.edu <rohankharwadkar@ufl.edu>
2018-10-05 23:30:05 -0400

    Added Create Post Page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import NavBar from './components/NavBar.js';
 import Home from './views/Home.js';
 import Feed from './views/feed.js';
+import CreatePost from './components/CreatePost.js'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 const App = () => (
@@ -15,6 +16,9 @@ const App = () => (
       </div>
       <div>
         <Route exact path="/" component={Home}/>
+      </div>
+      <div>
+        <Route exact path="/CreatePost" component={CreatePost}/>
       </div>
   </div>
 )
