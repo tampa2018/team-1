1ada21bf0cf4f75868bf4de5559071643076a711
rohankharwadkar@ufl.edu <rohankharwadkar@ufl.edu>
2018-10-05 22:01:22 -0400

    Created Feed and Posts View

---
+++ src/views/feed.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import SimpleCard from '../components/post.js'
+
+export default class Feed extends React.Component {
+    render () {
+        return (
+        <div>
+            <div>
+                <SimpleCard/>
+            </div>
+        </div>
+
+        )
+    }
+}
