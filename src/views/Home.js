1d16acf45bec4579096c3e31fb2db2f3c9adf17e
rohankharwadkar@ufl.edu <rohankharwadkar@ufl.edu>
2018-10-06 08:57:03 -0400

    Formatted Home Page

--- src/views/Home.js
+++ src/views/Home.js
@@ -18,7 +18,7 @@ const styles = theme => ({
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 2,
     paddingBottom: theme.spacing.unit * 2,
-    height: "80vh",
+    height: "95vh",
     width: '80%',
     marginLeft: 'auto',
     marginRight: 'auto',
@@ -31,6 +31,10 @@ const styles = theme => ({
       fontSize: "32px",
       textAlign: "center",
   },
+  body: {
+    fontSize: "17px",
+    textAlign: "center",
+  }
 });
 
 function Home(props) {
@@ -58,8 +62,22 @@ function Home(props) {
                     their own challenges - they just need low-barrier
                     methods to join together and address core issues.
                 </Typography>
+                <br/>
+                <Typography className={classes.header} component="p">
+                    <b>Join the conversation</b>
+                </Typography>
+                <br/>
+                <center><Facebook /></center>
+                <br/>
+                <Typography className={classes.body} component="p">
+                Our 100 Great Ideas campaigns are online conversations where we invite 
+                anyone and everyone to share their opinion about the best way to solve 
+                a pressing community issue. At the end of the campaigns, we synthesize 
+                all ideas into a report which we share broadly. We then bring the top 
+                ideas to the decision-makers in the community who have the ability to 
+                implement them.
+                </Typography>              
             </Grid>
-            <center><Facebook /></center>
         </Grid>
       </Paper>
       </MuiThemeProvider>
