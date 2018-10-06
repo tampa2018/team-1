dc672425a52a639e529cf6f35ab683c22ae6579f
rohankharwadkar@ufl.edu <rohankharwadkar@ufl.edu>
2018-10-06 07:45:11 -0400

    Created Contact Page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar.js';
 import Home from './views/Home.js';
 import Feed from './views/feed.js';
 import CreatePost from './components/CreatePost.js'
+import Contact from './views/Contact.js';
 import {
   BrowserRouter as Router,
   Route,
@@ -20,6 +21,7 @@ const App = () => (
           <NavBar/>
           <Switch>
             <Route path="/Feed" component={Feed}/>
+            <Route path="/Contact" component={Contact}/>
             <Route exact path="/" component={Home}/>
             <Route exact path="/CreatePost" component={CreatePost}/>
             <Route component={InvalidPage} status={404}/>
