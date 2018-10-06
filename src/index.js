1693a0e87b4baf9a05ab2531effc7ffb5b55ed5f
Alex Hartley <a.hartley@ufl.edu>
2018-10-05 23:51:02 -0400

    Patched 404 Error

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import CreatePost from './components/CreatePost.js'
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
+  Switch
 } from 'react-router-dom'
 import InvalidPage from './views/404.js'
 
@@ -21,10 +22,12 @@ const App = () => (
             <li><Link to="/Feed">Feed</Link></li>
             <li><Link to="/CreatePost">Create Post</Link></li>
           </ul>
-          <Route path="/Feed" component={Feed}/>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/CreatePost" component={CreatePost}/>
-          <Route path="*" component={NoMatch} status={404}/>
+          <Switch>
+            <Route path="/Feed" component={Feed}/>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/CreatePost" component={CreatePost}/>
+            <Route component={InvalidPage} status={404}/>
+          </Switch>
           </div>
       </Router>
   </div>
