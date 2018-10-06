564d6b7f65a2acaa2f8870cfe7d7b3279ac8eece
Bhavika <bhavikaboga7@gmail.com>
2018-10-06 08:19:12 -0400

    Implemented auto increment

--- dbscript.sql
+++ dbscript.sql
@@ -40,14 +40,14 @@ create table campaigns(
 );
 
 create table posts(
-	post_id varchar(100) not null,
+	post_id INT auto_increment not null,
 	fbid varchar(20) not null,
     post_subject varchar(500) not null,
     body varchar(1000),
     num_likes INT, 
     num_dislikes INT,
-    time_stamp varchar(100),
     category varchar(100),
+    time_stamp varchar(100),
     campaign_year varchar(8),
     primary key (post_id),
     foreign key (fbid) references users(fbid) on delete cascade,
@@ -55,9 +55,9 @@ create table posts(
   );
 
 create table comments(
+	comment_id INT auto_increment not null,
 	fbid varchar(20) not null,
-    post_id varchar(100) not null,
-    comment_id varchar(100) not null,
+    post_id INT not null,
     body varchar(1000),
     time_stamp varchar(100),
     primary key (comment_id),
@@ -79,14 +79,14 @@ INSERT into campaigns values("2018", "It's our 2018 campaign", "Get excited!");
 
 INSERT into experts values("2", "JPMorgan", "I work at JPMorgan I swear");
 
-INSERT into posts values("1", "1", "Who needs more coffee?", "me", 1000, 4, "someone give me coffee", "time", "2018");
-INSERT into posts values("2", "1", "I ate too many cookies", "many regrets", 1000, 4, "cookies", "time", "2018");
-INSERT into posts values("3", "2", "ideas ideas", "so much description", 1000, 4, "ideas", "time", "2018");
-INSERT into posts values("4", "3", "idea1", "body1", 1000, 4, "category1", "time", "2018");
-INSERT into posts values("5", "5", "idea2", "body2", 1000, 4, "category2", "time", "2018");
-INSERT into posts values("6", "6", "idea3", "body2", 1000, 4, "category3", "time", "2018");
+INSERT into posts(fbid, post_subject, body, num_likes, num_dislikes, category, time_stamp, campaign_year) values("1", "Who needs more coffee?", "me", 1000, 4, "someone give me coffee", "time", "2018");
+INSERT into posts(fbid, post_subject, body, num_likes, num_dislikes, category, time_stamp, campaign_year) values("1", "I ate too many cookies", "many regrets", 1000, 4, "cookies", "time", "2018");
+INSERT into posts(fbid, post_subject, body, num_likes, num_dislikes, category, time_stamp, campaign_year) values("2", "ideas ideas", "so much description", 1000, 4, "ideas", "time", "2018");
+INSERT into posts(fbid, post_subject, body, num_likes, num_dislikes, category, time_stamp, campaign_year) values("3", "idea1", "body1", 1000, 4, "category1", "time", "2018");
+INSERT into posts(fbid, post_subject, body, num_likes, num_dislikes, category, time_stamp, campaign_year) values("5", "idea2", "body2", 1000, 4, "category2", "time", "2018");
+INSERT into posts(fbid, post_subject, body, num_likes, num_dislikes, category, time_stamp, campaign_year) values("6", "idea3", "body2", 1000, 4, "category3", "time", "2018");
 
-INSERT into comments values("2", "1", "1", "sammeeeee", "time");
-INSERT into comments values("3", "3", "2", "oh wow what a great idea", "time");
-INSERT into comments values("6", "2", "3", "lol me", "time");
-INSERT into comments values("4", "6", "4", "more idea contribution, look at me go", "time");
+INSERT into comments(fbid, post_id, body, time_stamp) values("2", "1", "sammeeeee", "time");
+INSERT into comments(fbid, post_id, body, time_stamp) values("3", "3", "oh wow what a great idea", "time");
+INSERT into comments(fbid, post_id, body, time_stamp) values("6", "2", "lol me", "time");
+INSERT into comments(fbid, post_id, body, time_stamp) values("4", "6", "more idea contribution, look at me go", "time");
