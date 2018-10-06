12b6d210898c2c1ac471b189cf88811ae73301b0
Alex Hartley <a.hartley@ufl.edu>
2018-10-05 23:44:00 -0400

    Improving NavBar

--- src/index.js
+++ src/index.js
@@ -4,28 +4,35 @@ import NavBar from './components/NavBar.js';
 import Home from './views/Home.js';
 import Feed from './views/feed.js';
 import CreatePost from './components/CreatePost.js'
-import { BrowserRouter, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import InvalidPage from './views/404.js'
 
 const App = () => (
   <div>
-      <div>
-        <NavBar/>
-      </div>
-      <div>
-        <Route path="/Feed" component={Feed}/>
-      </div>
-      <div>
-        <Route exact path="/" component={Home}/>
-      </div>
-      <div>
-        <Route exact path="/CreatePost" component={CreatePost}/>
-      </div>
+      <Router>
+        <div>
+          <NavBar/>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/Feed">Feed</Link></li>
+            <li><Link to="/CreatePost">Create Post</Link></li>
+          </ul>
+          <Route path="/Feed" component={Feed}/>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/CreatePost" component={CreatePost}/>
+          <Route path="/" component={InvalidPage}/>
+          </div>
+      </Router>
   </div>
 )
 
 ReactDOM.render((
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>),
+    </Router>),
     document.getElementById('root')
   );
