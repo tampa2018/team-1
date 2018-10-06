62fb3a69d9edccb1f1971ed72496b16e8d88653f
Alex Hartley <a.hartley@ufl.edu>
2018-10-05 20:22:10 -0400

    Home Page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from './components/NavBar.js';
+import Home from './views/Home.js';
 
 
-function App() {
-  return (
-    <NavBar/>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="NavBar">
+          <NavBar/>
+        </div>
+        <div className="Page">
+          <Home/>
+        </div>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
